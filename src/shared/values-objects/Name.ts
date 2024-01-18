0929ed36e5970eb347cef046630c9bfe38d25918
dyhalmeida <dyhalmeida@gmail.com>
2024-01-18 10:48:50 -0300

    fix: change name validation regex

--- src/shared/values-objects/Name.ts
+++ src/shared/values-objects/Name.ts
@@ -12,7 +12,7 @@ export class Name {
             Validator.isSmallerThan({ value: this.name, size: 4, errorMessage: Errors.SMALL_NAME }),
             Validator.isBiggerThan({ value: this.name, size: 120, errorMessage: Errors.BIG_NAME }),
             Validator.isEmpty({ value: this.name.split(' ')[1], errorMessage: Errors.NAME_WITHOUT_LAST_NAME }),
-            Validator.isValidRegex({ value: this.name, regex: /^[a-zA-ZÀ-ú\s]+$/, errorMessage: Errors.NAME_WITH_INVALID_CHARACTERS })
+            Validator.isValidRegex({ value: this.name, regex: /^[a-zA-ZÀ-ú'\.-\s]*$/, errorMessage: Errors.NAME_WITH_INVALID_CHARACTERS })
         )
         if (errors !== null) throw errors
     }
